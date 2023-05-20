4ffab52077c6c135b9a3426c2f8bbd7c6525f8df
Eduardo Silveira <60801786+EduardoSilveira6657@users.noreply.github.com>
2023-05-20 13:30:34 -0300

    update: basic Player movement Script

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -6,16 +6,49 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeedMultiplier = 5f;
     [SerializeField] Rigidbody2D playerRigidBody;
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] GameObject sideWaysModel;
+    [SerializeField] GameObject upWayModel;
+    [SerializeField] GameObject downWayModel;
     
-    Vector2 movementInput;
+    Vector2 _movementInput;
+    int _verticalInputParam;
+    int _horizontalInputParam;
+    int _movementSpeedParam;
+
+    void Awake()
+    {
+        _verticalInputParam = Animator.StringToHash("Vertical Input");
+        _horizontalInputParam = Animator.StringToHash("Horizontal Input");
+        _movementSpeedParam = Animator.StringToHash("Movement Speed");
+    }
+
     void Update()
     {
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
+        _movementInput.x = Input.GetAxisRaw("Horizontal");
+        _movementInput.y = Input.GetAxisRaw("Vertical");
+        
+        if(_movementInput.y > 0.1f) {
+            sideWaysModel.SetActive(false);
+            upWayModel.SetActive(true);
+            downWayModel.SetActive(false);
+        } else if(_movementInput.y < -0.1f) {
+            sideWaysModel.SetActive(false);
+            upWayModel.SetActive(false);
+            downWayModel.SetActive(true);
+        } else {
+            sideWaysModel.SetActive(true);
+            upWayModel.SetActive(false);
+            downWayModel.SetActive(false);
+        }
+
+        playerAnimator.SetFloat(_verticalInputParam, _movementInput.x);
+        playerAnimator.SetFloat(_horizontalInputParam, _movementInput.y);
+        playerAnimator.SetFloat(_movementSpeedParam, _movementInput.sqrMagnitude);
     }
     
     void FixedUpdate()
     {
-        playerRigidBody.MovePosition(playerRigidBody.position + movementInput * movementSpeedMultiplier * Time.fixedDeltaTime);
+        playerRigidBody.MovePosition(playerRigidBody.position + _movementInput * movementSpeedMultiplier * Time.fixedDeltaTime);
     }
 }
